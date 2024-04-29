634ab5fc0e9d988de8ddc94c7357cffd5b1aa6da
Karthik2502 <105701174+TrainerKarthi@users.noreply.github.com>
2024-04-29 20:23:32 +0530

    Add files via upload

---
+++ user-management-system/src/main/java/com/org/controller/LoginServlet.java
@@ -0,0 +1,37 @@
+package com.org.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.org.dao.UserDao;
+import com.org.dto.User;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		String email = req.getParameter("email");
+		String password = req.getParameter("password");
+
+		UserDao dao = new UserDao();
+		User user = dao.fetchUserByEmailAndPassword(email, password);
+
+		HttpSession session = req.getSession();
+		if (user != null) {
+			session.setAttribute("userObj", user);	
+			resp.sendRedirect("home.jsp");
+		} else {
+			
+			session.setAttribute("fail", "Invalid Credentials");
+			resp.sendRedirect("login.jsp");
+		}
+	}
+}
